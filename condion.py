6cdd61b685a1aa8687fd3d53756792408dbccf62
sowrov <sowrovsarkar63@gmail.com>
2020-11-23 11:47:39 +0600

    Just practice some fundamental concept of python programming language

---
+++ condion.py
@@ -0,0 +1,20 @@
+# numbers = [6,5,3,8,4,2,5,4]
+# sam = 0
+#
+# for val in numbers:
+#     sam = sam+val
+#
+# print("sum is ",sam)
+
+# genre =['pop','rock','jazz']
+#
+#
+# for i in range(len(genre)):
+#     print("I like ",genre[i])
+
+
+digits = [0, 1, 5]
+for i in digits:
+    print(i)
+else:
+    print("No items left")
